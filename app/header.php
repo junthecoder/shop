573f4b06326261bc5ddbd812428af5968bb0369c
junthecoder <>
2023-01-31 15:23:02 +0900

    Implement logout

--- app/header.php
+++ app/header.php
@@ -7,13 +7,18 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
 <?php if (isset($_SESSION['user'])): ?>
-            <a class="nav-link" href="login.php"><?= $_SESSION['user']['name'] ?> さん</a>
-<?php else: ?>
-            <a class="nav-link" href="login.php">ログイン</a>
-<?php endif ?>
+          <li class="nav-item">
+            <div class="navbar-text"><?= $_SESSION['user']['name'] ?> さん</div>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="logout.php">ログアウト</a>
+          </li>
+<?php else: ?>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">ログイン</a>
+            </li>
+<?php endif ?>
           <li class="nav-item">
             <a class="nav-link" href="cart.php">カート</a>
           </li>
