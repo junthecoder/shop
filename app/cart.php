f93169700859b4a3c08a2d6379c0d3748a068a15
junthecoder <>
2023-02-01 15:39:53 +0900

    Implement: changing the count of an item in the cart

--- app/cart.php
+++ app/cart.php
@@ -16,7 +16,15 @@ try {
     if (isset($_POST['action'])) {
         switch ($_POST['action']) {
             case 'add':
-                $_SESSION['cart'][] = get_item_by_id($dbh, $_POST['item_id']);
+                $_SESSION['cart'][] = ['id' => $_POST['item_id'], 'count' => 1];
+                break;
+            case 'change_count':
+                foreach ($_SESSION['cart'] as $key => $item) {
+                    if ($item['id'] == $_POST['item_id']) {
+                        $_SESSION['cart'][$key]['count'] = $_POST['count'];
+                        break;
+                    }
+                  }
                 break;
             case 'delete':
                 $_SESSION['cart'] = array_filter($_SESSION['cart'], fn($item) => $item['id'] != $_POST['item_id']);
@@ -24,8 +32,10 @@ try {
         }
     }
 
-    $ids = array_map(fn($item) => $item['id'], $_SESSION['cart']);
-    $items = get_items_by_ids($dbh, $ids);
+    $items = [];
+    foreach ($_SESSION['cart'] as $cart_item) {
+        $items[$cart_item['id']] = get_item_by_id($dbh, $cart_item['id']);
+    }
 } catch (Exception $e) {
     print $e;
 } finally {
@@ -36,6 +46,11 @@ try {
 
 <?php include_template('pre_body.php', ['title' => 'ショッピングカート']) ?>
   <?php include 'header.php' ?>
+  <script>
+    function changeCount(event) {
+      event.target.getAttribute('data-itemid');
+    }
+  </script>
   <div class="container m-5">
     <div class="row">
       <h3 class="p-3">ショッピングカート</h3>
@@ -45,25 +60,42 @@ try {
     <?php else: ?>
       <div class="row">
         <div class="col">
-          <?php foreach ($items as $item): ?>
-          <div class="card">
-            <div class="d-flex justify-content-between">
-              <img src="/images/200x200.png" class="card-img-left" alt="<?= $item['name'] ?>" style="width: 160px; height: 160px">
-              <div class="card-body">
-                <div class="d-flex justify-content-between">
-                  <h5 class="card-title"><?= $item['name'] ?></h5>
-                  <p class="">¥<?= $item['price'] ?></p>
-                </div>
-                <div class="float-end">
+          <?php foreach ($_SESSION['cart'] as $cart_item): ?>
+            <?php $item = $items[$cart_item['id']]; ?>
+            <div class="card">
+              <div class="d-flex justify-content-between">
+                <img src="/images/200x200.png" class="card-img-left" alt="<?= $item['name'] ?>" style="width: 160px; height: 160px">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between">
+                    <h5 class="card-title"><?= $item['name'] ?></h5>
+                    <p class="">¥<?= $item['price'] * $cart_item['count'] ?></p>
+                  </div>
                   <form action="cart.php" method="post">
-                    <input type="hidden" name="action" value="delete">
-                    <input type="hidden" name="item_id" value="<?= $item['id'] ?>">
-                    <button type="submit" class="btn btn-danger">削除</button>
+                    <input type="hidden" name="action" value="change_count">
+                    <input type="hidden" name="item_id" value="<?= $cart_item['id'] ?>">
+                    <select name="count" class="form-select form-select-sm" style="width: 5em" value="<?= $cart_item['count'] ?>" onchange="this.form.submit()">
+                      <?php for ($i = 1; $i <= 10; ++$i): ?>
+                        <option
+                          value="<?= $i ?>"
+                          <?php if ($i == $cart_item['count']): ?>
+                              selected="selected"
+                          <?php endif ?>
+                        >
+                          <?= $i ?>
+                        </option>
+                      <?php endfor ?>
+                    </select>
                   </form>
+                  <div class="float-end">
+                    <form action="cart.php" method="post">
+                      <input type="hidden" name="action" value="delete">
+                      <input type="hidden" name="item_id" value="<?= $item['id'] ?>">
+                      <button type="submit" class="btn btn-danger">削除</button>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
           <?php endforeach ?>
         </div>
       </div>
