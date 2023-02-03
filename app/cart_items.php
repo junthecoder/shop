546323f2261298c14349277b25bb22ff6f06cb84
junthecoder <>
2023-02-03 14:12:11 +0900

    Format cart_items.php

--- app/cart_items.php
+++ app/cart_items.php
@@ -10,49 +10,48 @@ foreach ($_SESSION['cart'] as $cart_item) {
 
 ?>
 
-
-    <div class="row">
-      <div class="col">
-        <?php foreach ($_SESSION['cart'] as $cart_item): ?>
-          <?php $item = $items[$cart_item['id']]; ?>
-          <div class="card">
+<div class="row">
+  <div class="col">
+    <?php foreach ($_SESSION['cart'] as $cart_item): ?>
+      <?php $item = $items[$cart_item['id']]; ?>
+      <div class="card">
+        <div class="d-flex justify-content-between">
+          <img src="/images/200x200.png" class="card-img-left" alt="<?= $item['name'] ?>" style="width: 160px; height: 160px">
+          <div class="card-body">
             <div class="d-flex justify-content-between">
-              <img src="/images/200x200.png" class="card-img-left" alt="<?= $item['name'] ?>" style="width: 160px; height: 160px">
-              <div class="card-body">
-                <div class="d-flex justify-content-between">
-                  <h5 class="card-title"><?= $item['name'] ?></h5>
-                  <p class="">¥<?= $item['price'] * $cart_item['count'] ?></p>
-                </div>
-                <?php if ($count_changeable): ?>
-                  <form action="cart.php" method="post">
-                    <input type="hidden" name="action" value="change_count">
-                    <input type="hidden" name="item_id" value="<?= $cart_item['id'] ?>">
-                    <select name="count" class="form-select form-select-sm" style="width: 5em" value="<?= $cart_item['count'] ?>" onchange="this.form.submit()">
-                      <?php for ($i = 1; $i <= 10; ++$i): ?>
-                        <option
-                          value="<?= $i ?>"
-                          <?php if ($i == $cart_item['count']): ?>
-                              selected="selected"
-                          <?php endif ?>
-                        >
-                          <?= $i ?>
-                        </option>
-                      <?php endfor ?>
-                    </select>
-                  </form>
-                <?php endif ?>
-                <?php if ($deletable): ?>
-                  <div class="float-end">
-                    <form action="cart.php" method="post">
-                      <input type="hidden" name="action" value="delete">
-                      <input type="hidden" name="item_id" value="<?= $item['id'] ?>">
-                      <button type="submit" class="btn btn-danger">削除</button>
-                    </form>
-                  </div>
-                <?php endif ?>
-              </div>
+              <h5 class="card-title"><?= $item['name'] ?></h5>
+              <p class="">¥<?= $item['price'] * $cart_item['count'] ?></p>
             </div>
+            <?php if ($count_changeable): ?>
+              <form action="cart.php" method="post">
+                <input type="hidden" name="action" value="change_count">
+                <input type="hidden" name="item_id" value="<?= $cart_item['id'] ?>">
+                <select name="count" class="form-select form-select-sm" style="width: 5em" value="<?= $cart_item['count'] ?>" onchange="this.form.submit()">
+                  <?php for ($i = 1; $i <= 10; ++$i): ?>
+                    <option
+                      value="<?= $i ?>"
+                      <?php if ($i == $cart_item['count']): ?>
+                          selected="selected"
+                      <?php endif ?>
+                    >
+                      <?= $i ?>
+                    </option>
+                  <?php endfor ?>
+                </select>
+              </form>
+            <?php endif ?>
+            <?php if ($deletable): ?>
+              <div class="float-end">
+                <form action="cart.php" method="post">
+                  <input type="hidden" name="action" value="delete">
+                  <input type="hidden" name="item_id" value="<?= $item['id'] ?>">
+                  <button type="submit" class="btn btn-danger">削除</button>
+                </form>
+              </div>
+            <?php endif ?>
           </div>
-        <?php endforeach ?>
+        </div>
       </div>
-    </div>
+    <?php endforeach ?>
+  </div>
+</div>
