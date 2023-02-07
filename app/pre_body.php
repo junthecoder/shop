6e100ef1d5414c24c79d6f98c93ddd2dd067b6a7
junthecoder <>
2023-02-07 14:29:20 +0900

    Move jQuery into the head

--- app/pre_body.php
+++ app/pre_body.php
@@ -6,5 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src=" https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js" type="text/javascript"></script>
   </head>
   <body>
