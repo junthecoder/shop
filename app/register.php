f43f24ff33d680d9a2839c38c08c7f6a7515ca7c
junthecoder <>
2023-02-07 15:17:07 +0900

    Prevent submit if inputs are invalid

--- app/register.php
+++ app/register.php
@@ -98,25 +98,31 @@ if ($register) {
   <script>
     'use strict';
 
-    $('#name-input')[0].addEventListener('input', () => {
+    function validateName() {
       let name = $('#name-input').val();
+
       let errors = [];
       if (!/\S/.test(name)) {
         errors.push('名前を入力してください');
       }
       $('#name-feedback').html(errors.join('<br>'));
-    });
 
-    $('#email-input')[0].addEventListener('input', () => {
+      return errors.length === 0;
+    }
+
+    function validateEmail() {
       let email = $('#email-input').val();
+
       let errors = [];
       if (!/^\S+@\S+$/.test(email)) {
         errors.push('メールアドレスを入力してください');
       }
       $('#email-feedback').html(errors.join('<br>'));
-    });
 
-    $('#password-input')[0].addEventListener('input', () => {
+      return errors.length === 0;
+    }
+
+    function validatePassword() {
       let password = $('#password-input').val();
 
       let errors = [];
@@ -129,18 +135,34 @@ if ($register) {
       if (!/\d/.test(password)) {
         errors.push('数字が含まれていません');
       }
-
       $('#password-feedback').html(errors.join('<br>'));
-    });
 
-    $('#password-check-input')[0].addEventListener('input', () => {
+      return errors.length === 0;
+    }
+
+    function validatePasswordCheck() {
       let password = $('#password-input').val();
       let passwordCheck = $('#password-check-input').val();
+
       let errors = [];
       if (password !== passwordCheck) {
         errors.push('パスワードが一致しません')
       }
       $('#password-check-feedback').html(errors.join('<br>'));
+
+      return errors.length === 0;
+    }
+
+    $('#name-input')[0].addEventListener('input', validateName);
+    $('#email-input')[0].addEventListener('input', validateEmail);
+    $('#password-input')[0].addEventListener('input', validatePassword);
+    $('#password-check-input')[0].addEventListener('input', validatePasswordCheck);
+
+    $('#registration-form')[0].addEventListener('submit', event => {
+      if (!(validateName() & validateEmail() & validatePassword() & validatePasswordCheck())) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
     });
   </script>
 <?php include_template('post_body.php') ?>
