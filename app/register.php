3a9e5a45d719ef44f0d7e51046feca8323ee8402
junthecoder <>
2023-02-07 15:06:43 +0900

    Add JavaScript validation to the registrtion form

--- app/register.php
+++ app/register.php
@@ -40,7 +40,7 @@ if ($register) {
       <p>ユーザ登録を完了しました。</p>
       <a href="/">トップページへ</a>
     <?php else: ?>
-      <form class="container p-5" style="width: 450px;" action="register.php" method="post">
+      <form class="container p-5 needs-validation" style="width: 450px;" action="register.php" method="post" id="registration-form" novalidate>
         <div class="row mb-4">
           <h3>ユーザ登録</h3>
         </div>
@@ -50,7 +50,8 @@ if ($register) {
             <p><?= $post['name'] ?></p>
             <input type="hidden" name="name" value="<?= $post['name'] ?>">
           <?php else: ?>
-            <input type="text" name="name" class="form-control" id="name-input" placeholder="山田 太郎">
+            <input type="text" name="name" class="form-control" id="name-input" placeholder="山田 太郎" required>
+            <div class="invalid-feedback d-block" id="name-feedback"></div>
           <?php endif ?>
         </div>
         <div class="row mb-3">
@@ -59,7 +60,8 @@ if ($register) {
             <p><?= $post['email'] ?></p>
             <input type="hidden" name="email" value="<?= $post['email'] ?>">
           <?php else: ?>
-            <input type="email" name="email" class="form-control" id="email-input" placeholder="name@example.com">
+            <input type="email" name="email" class="form-control" id="email-input" placeholder="name@example.com" required>
+            <div class="invalid-feedback d-block" id="email-feedback"></div>
           <?php endif ?>
         </div>
         <div class="row mb-3">
@@ -68,14 +70,16 @@ if ($register) {
             <p>****</p>
             <input type="hidden" name="password" value="<?= $post['password'] ?>">
           <?php else: ?>
-            <input type="password" name="password" class="form-control" id="password-input" placeholder="8文字以上">
+            <input type="password" name="password" class="form-control" id="password-input" placeholder="8文字以上" required>
+            <div class="invalid-feedback d-block" id="password-feedback"></div>
           <?php endif ?>
         </div>
       <?php if ($check): ?>
       <?php else: ?>
           <div class="row mb-3">
             <label for="password-check-input" class="form-label">もう一度パスワードを入力してください</label>
-            <input type="password" name="password-check" class="form-control" id="password-check-input">
+            <input type="password" name="password-check" class="form-control" id="password-check-input" required>
+            <div class="invalid-feedback d-block" id="password-check-feedback"></div>
           </div>
       <?php endif ?>
         <div class="row my-4">
@@ -84,11 +88,59 @@ if ($register) {
             <input type="submit" class="btn btn-primary" value="登録する">
           <?php else: ?>
             <input type="hidden" name="check" value="1">
-            <input type="submit" class="btn btn-primary" value="次に進む">
+            <input type="submit" class="btn btn-primary" name="submit" value="次に進む">
           <?php endif ?>
         </div>
       </form>
     <?php endif ?>
   </div>
-<?php include_template('post_body.php') ?>
 
+  <script>
+    'use strict';
+
+    $('#name-input')[0].addEventListener('input', () => {
+      let name = $('#name-input').val();
+      let errors = [];
+      if (!/\S/.test(name)) {
+        errors.push('名前を入力してください');
+      }
+      $('#name-feedback').html(errors.join('<br>'));
+    });
+
+    $('#email-input')[0].addEventListener('input', () => {
+      let email = $('#email-input').val();
+      let errors = [];
+      if (!/^\S+@\S+$/.test(email)) {
+        errors.push('メールアドレスを入力してください');
+      }
+      $('#email-feedback').html(errors.join('<br>'));
+    });
+
+    $('#password-input')[0].addEventListener('input', () => {
+      let password = $('#password-input').val();
+
+      let errors = [];
+      if (password.length === 0) {
+        errors.push('パスワードを入力してください');
+      }
+      if (!/[a-zA-Z]/.test(password)) {
+        errors.push('アルファベットが含まれていません');
+      }
+      if (!/\d/.test(password)) {
+        errors.push('数字が含まれていません');
+      }
+
+      $('#password-feedback').html(errors.join('<br>'));
+    });
+
+    $('#password-check-input')[0].addEventListener('input', () => {
+      let password = $('#password-input').val();
+      let passwordCheck = $('#password-check-input').val();
+      let errors = [];
+      if (password !== passwordCheck) {
+        errors.push('パスワードが一致しません')
+      }
+      $('#password-check-feedback').html(errors.join('<br>'));
+    });
+  </script>
+<?php include_template('post_body.php') ?>
