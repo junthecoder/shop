c4177fbba3c46f917f83ae9853f132925d11f314
junthecoder <>
2023-02-08 13:52:49 +0900

    Increment the count by 1 if the item to be added is already in the cart

--- app/cart.php
+++ app/cart.php
@@ -7,32 +7,37 @@ if (!isset($_SESSION['cart'])) {
   $_SESSION['cart'] = [];
 }
 
-if ($_POST) {
-    try {
-        $db = new Database;
-
-        if (isset($_POST['action'])) {
-            switch ($_POST['action']) {
-                case 'add':
-                    $_SESSION['cart'][] = ['id' => $_POST['item_id'], 'count' => 1];
-                    break;
-                case 'change_count':
-                    foreach ($_SESSION['cart'] as $key => $item) {
-                        if ($item['id'] == $_POST['item_id']) {
-                            $_SESSION['cart'][$key]['count'] = $_POST['count'];
-                            break;
-                        }
-                    }
-                    break;
-                case 'delete':
-                    $_SESSION['cart'] = array_filter($_SESSION['cart'], fn($item) => $item['id'] != $_POST['item_id']);
-                    break;
-            }
+function find_cart_item_key_by_id($id)
+{
+    foreach ($_SESSION['cart'] as $key => $item) {
+        if ($item['id'] == $_POST['item_id']) {
+            return $key;
         }
-    } catch (Exception $e) {
-        print $e;
-    } finally {
-        $db = null;
+    }
+    return false;
+}
+
+if ($_POST) {
+    $db = new Database;
+
+    if (isset($_POST['action'])) {
+        switch ($_POST['action']) {
+            case 'add':
+                $key = find_cart_item_key_by_id($_POST['item_id']);
+                if ($key === false) {
+                    $_SESSION['cart'][] = ['id' => $_POST['item_id'], 'count' => 1];
+                } else {
+                    ++$_SESSION['cart'][$key]['count'];
+                }
+                break;
+            case 'change_count':
+                $key = find_cart_item_key_by_id($_POST['item_id']);
+                $_SESSION['cart'][$key]['count'] = $_POST['count'];
+                break;
+            case 'delete':
+                $_SESSION['cart'] = array_filter($_SESSION['cart'], fn($item) => $item['id'] != $_POST['item_id']);
+                break;
+        }
     }
 
     redirect();
