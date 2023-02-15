9a9f61068e762cc627742d0b595a8d3f0183a7aa
junthecoder <>
2023-02-15 22:17:24 +0900

    Fix(header): show the number of items in cart on every page

--- app/index.php
+++ app/index.php
@@ -33,15 +33,8 @@ switch ($sort_type) {
 }
 $items = $db->get_items_batch($num_items_per_page * ($current_page - 1), $num_items_per_page, $order);
 
-if (isset($_SESSION['cart'])) {
-    $num_cart_items = array_reduce($_SESSION['cart'], fn ($sum, $item) => $sum + $item['count'], 0);
-} else {
-    $num_cart_items = 0;
-}
-
 echo load_twig()->render('index.html.twig', [
     'items' => $items,
-    'num_cart_items' => $num_cart_items,
     'num_items_per_page' => $num_items_per_page,
     'num_pages' => $num_pages,
     'current_page' => $current_page,
